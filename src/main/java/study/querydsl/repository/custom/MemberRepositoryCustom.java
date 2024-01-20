4af56c76d5c993ec01a9183c162d396790963dae
coPpark <qkrwjdtn963@gmail.com>
2024-01-21 01:01:17 +0900

    feature: querydsl 학습 코드 (#1)
    * local 프로필 및 test yml 추가
    
    * mock 데이터 삽입
    
    * jpaQueryFactory 전역설정
    
    * test 코드
    
    * querydsl study
    
    ---------
    
    Co-authored-by: jeongsu.park <jeongsu.park@seculayer.com>

---
+++ src/main/java/study/querydsl/repository/custom/MemberRepositoryCustom.java
@@ -0,0 +1,10 @@
+package study.querydsl.repository.custom;
+
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+    List<MemberTeamDto> search(MemberSearchCondition condition);
+}
---
+++ src/main/java/study/querydsl/repository/query/MemberQueryRepository.java
@@ -0,0 +1,62 @@
+package study.querydsl.repository.query;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
+
+import java.util.List;
+
+import static org.springframework.util.StringUtils.hasText;
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+/**
+ * 근데 만약 조회용 쿼리나 별도의 쿼리가 너무 복잡하고 특화된 쿼리면
+ * custom으로 굳이 안하고 하나의 class파일에 만드는걸 권장
+ */
+@Repository
+public class MemberQueryRepository {
+    private final JPAQueryFactory queryFactory;
+    public MemberQueryRepository(EntityManager em){
+
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
+}
