f0ade5575ad07800085d81bb77d8844afe830f38
jeongsu.park <jeongsu.park@seculayer.com>
2024-01-21 00:54:48 +0900

    querydsl study

---
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package study.querydsl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.querydsl.entity.Member;
+import study.querydsl.repository.custom.MemberRepositoryCustom;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+    List<Member> findByUsername(String username);
+
+
+}
